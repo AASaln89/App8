e5159ab4b728a86cf9e93ae221dec49c4b539db2
Andrey Salnikov <kvanta13@yandex.ru>
2022-03-05 00:39:48 +0300

    123

---
+++ 8.1.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path1 = "D:/Log.txt";
+            using (StreamReader sr = new StreamReader(path1))
+            {
+                Console.Write(sr.ReadToEnd());
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ 8.1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string path = "Logs";
+            //if (Directory.Exists(path))
+            //{
+            //    Directory.CreateDirectory(path);
+            //}
+            //DirectoryInfo directory = new DirectoryInfo(path);
+            //if (!directory.Exists)
+            //{
+            //    directory.Create();
+            //}
+            string path1 = "D:/Log.txt";
+            if (!File.Exists(path1))
+            {
+                File.Create(path1);
+            }
+            using (StreamWriter streamWriter = new StreamWriter(path1, true))
+            {
+                int[] array = new int[10];
+                Random random = new Random();
+                for (int i = 0; i < 10; i++)
+                {
+                    array[i] = random.Next(-5, 5);
+                    streamWriter.WriteLine("{0} ", array[i]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
